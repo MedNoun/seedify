99a975facbe9e4597ea739353b7c4c3b37dee602
sllim637 <slim.hammami1977@gmail.com>
2023-02-15 22:24:32 +0100

    fixing importing modules issues and read torrent file

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,10 @@
-"use strict";
-console.log("Hello world!");
+import * as fs from 'fs';
+import bencode from 'bencode';
+import URLParse from 'url-parse';
+function readFile(torrentFilePath) {
+    const torrentFileContent = fs.readFileSync(torrentFilePath);
+    const torrentFileContentDecoded = bencode.decode(torrentFileContent, "utf8");
+    console.log("the url is :", torrentFileContentDecoded.announce);
+    console.log("the parsed url is :", URLParse(torrentFileContentDecoded.announce));
+}
+readFile("src/torrent-files/t9.torrent");
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+import * as fs from 'fs';
+import bencode from 'bencode'
+import URLParse from 'url-parse';
+
+
+function readFile(torrentFilePath: string) {
+    const torrentFileContent = fs.readFileSync(torrentFilePath);
+    const torrentFileContentDecoded = bencode.decode(torrentFileContent,"utf8")
+    console.log("the url is :" , torrentFileContentDecoded.announce);
+    console.log("the parsed url is :",URLParse(torrentFileContentDecoded.announce));
+}
+readFile("src/torrent-files/t9.torrent")
