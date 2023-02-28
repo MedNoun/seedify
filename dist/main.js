f952abc8059a3bb203fefdf5f9aa14f3c3caa572
sllim637 <slim.hammami1977@gmail.com>
2023-02-28 19:00:48 +0100

    first module done

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,5 @@
 import { TrackerManager } from "./tracker-manager.js";
 const trackerManager = new TrackerManager("src/torrent-files/t9.torrent");
-trackerManager.httpConnectRequest().then((response) => {
-    console.log('walah khdemt aman');
-});
-// trackerManager.getPeers();
+// trackerManager.httpConnectRequest().then((response) => {console.log('walah khdemt aman');
+// })
+trackerManager.getPeers();
